bfcb2fb93ddd523f6a8499f34847fb8172cc957d
noobdy <htommyf12@gmail.com>
2024-05-30 16:13:16 +0800

    not working

--- packages/frontend/src/routes/projects/new.tsx
+++ packages/frontend/src/routes/projects/new.tsx
@@ -1,15 +1,25 @@
+import { Select } from "@kobalte/core/select";
 import { NumberField } from "@kobalte/core/number-field";
 import { TextField } from "@kobalte/core/text-field";
 import { action, redirect } from "@solidjs/router";
-import { createMemo, createSignal } from "solid-js";
+import { Show, createEffect, createMemo, createSignal } from "solid-js";
 import { Button } from "~/components/Button";
 import { useProjects } from "~/db";
+import { useAccount } from "~/hooks/useAccount";
 
 export default function () {
+    const [account] = useAccount();
     const [title, setTitle] = createSignal("");
     const [description, setDescription] = createSignal("");
     const [goal, setGoal] = createSignal(100);
+    const [address, setAddress] = createSignal<`0x${string}`>();
     const [, { add }] = useProjects();
+
+    createEffect(() => {
+        if (account.status === "connected") {
+            setAddress(account.address);
+        }
+    });
 
     const newProject = action(async (formData: FormData) => {
         const title = formData.get("title");
@@ -60,12 +70,12 @@ export default function () {
     });
 
     return (
-        <div class="mx-auto px-4 md:w-1/3">
+        <div class="mx-auto px-4 md:w-2/5">
             <h1 class="font-mono text-4xl">Create New Project</h1>
             <form
                 action={newProject}
                 method="post"
-                class="flex flex-col space-y-2"
+                class="flex w-full flex-col space-y-4"
             >
                 <TextField
                     name="title"
@@ -78,8 +88,8 @@ export default function () {
                     <TextField.Label class="font-mono">Title</TextField.Label>
                     <br />
                     <TextField.Input
-                        class="border bg-neutral-100 px-2 dark:bg-neutral-800
-                        dark:text-white"
+                        class="w-full border bg-neutral-100 px-2
+                        dark:bg-neutral-800 dark:text-white"
                     />
                 </TextField>
 
@@ -98,8 +108,8 @@ export default function () {
                     </TextField.Label>
                     <br />
                     <TextField.TextArea
-                        class="border bg-neutral-100 px-2 dark:bg-neutral-800
-                        dark:text-white"
+                        class="w-full border bg-neutral-100 px-2
+                        dark:bg-neutral-800 dark:text-white"
                     />
                 </TextField>
 
@@ -111,6 +121,7 @@ export default function () {
                     onRawValueChange={setGoal}
                     validationState={goal() > 0 ? "valid" : "invalid"}
                     required
+                    class="flex flex-col"
                 >
                     <NumberField.Label class="font-mono">
                         Goal
@@ -118,8 +129,8 @@ export default function () {
                     <NumberField.HiddenInput />
                     <div class="flex">
                         <NumberField.Input
-                            class="border bg-neutral-100 px-2
-                            dark:bg-neutral-800 dark:text-white"
+                            class="grow border bg-neutral-100
+                            px-2 dark:bg-neutral-800 dark:text-white"
                         />
                         <div class="flex flex-col">
                             <NumberField.IncrementTrigger
@@ -151,6 +162,48 @@ export default function () {
                         Please enter a value greater than 0
                     </NumberField.ErrorMessage>
                 </NumberField>
+
+                <Show when={account.status === "connected" && account} keyed>
+                    {account => (
+                        <Select
+                            defaultValue={account.address}
+                            options={[...account.addresses]}
+                            itemComponent={props => (
+                                <Select.Item item={props.item}>
+                                    <Select.ItemLabel>
+                                        {props.item.textValue}
+                                    </Select.ItemLabel>
+                                </Select.Item>
+                            )}
+                        >
+                            <Select.Label class="font-mono">
+                                Account
+                            </Select.Label>
+
+                            <div class="flex">
+                                <Select.Trigger
+                                    class="grow border bg-neutral-100
+                                    px-2 text-left dark:bg-neutral-800
+                                    dark:text-white"
+                                >
+                                    <Select.Value<`0x${string}`>>
+                                        {state => state.selectedOption()}
+                                    </Select.Value>
+                                </Select.Trigger>
+                            </div>
+
+                            <Select.Portal>
+                                <Select.Content
+                                    class="grow border bg-neutral-100
+                                    px-2 text-left dark:bg-neutral-800
+                                    dark:text-white"
+                                >
+                                    <Select.Listbox />
+                                </Select.Content>
+                            </Select.Portal>
+                        </Select>
+                    )}
+                </Show>
 
                 <div class="mx-auto">
                     <Button type="submit" disabled={disabled()}>
