f2e26d7fa9e9612d613b3e06ea74be3229ceb339
noobdy <htommyf12@gmail.com>
2024-06-12 00:30:25 +0800

    updated contract address

--- packages/frontend/src/wagmiConfig.ts
+++ packages/frontend/src/wagmiConfig.ts
@@ -29,6 +29,6 @@ const dev = import.meta.env.VITE_DEV_MODE === "true";
 
 export const contractAddress = dev
     ? "0x5FbDB2315678afecb367f032d93F642f64180aa3"
-    : "0xA1969927F894eAaA5f03F4C8Aa7AF693Ba04626F";
+    : "0xFA54F4e4677fD1595Ed19f1565CD37DD3B9e3E0A";
 
 export const config = dev ? hardhatConfig : xttConfig;
