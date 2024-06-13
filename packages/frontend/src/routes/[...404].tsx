50c5a66e0e289bb815e4100adfb76104fe097015
noobdy <htommyf12@gmail.com>
2024-06-14 06:56:03 +0800

    added 404

---
+++ packages/frontend/src/routes/[...404].tsx
@@ -0,0 +1,11 @@
+export default function () {
+    return (
+        <div class="flex w-full flex-col items-center py-40">
+            <h1 class="mb-10 font-mono text-7xl text-neutral-400 dark:text-neutral-600">
+                404
+            </h1>
+
+            <img src="https://media3.giphy.com/media/pkYigxymEkV44/200w.gif?cid=6c09b952hx2og7xxp6hm56h52ynzbgjoyyqfoox21eab5nxj&ep=v1_gifs_search&rid=200w.gif&ct=g" />
+        </div>
+    );
+}
